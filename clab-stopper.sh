1474daa0bdcb0105fa553f56803ca51a39208202
Shamen Snyder <shamen.snyder@gmail.com>
2024-08-22 11:37:00 -0400

    Update clab-stopper.sh

--- clab-stopper.sh
+++ clab-stopper.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+cd /home/ssnyder/clab-quickstart/
+
 # List all running labs
-running_labs=$(containerlab inspect --all | grep -oP '(?<=name: )\S+')
+running_labs=$(sudo containerlab inspect --all | grep -oP '\S+\.yml')
 
 # Check if there are any running labs
 if [ -z "$running_labs" ]; then
@@ -11,8 +13,10 @@ fi
 
 # Loop through each running lab and stop it
 for lab in $running_labs; do
+  # Strip '../' from the file path
+  lab=$(echo "$lab" | sed 's|^\.\./||')
   echo "Stopping lab: $lab"
-  containerlab destroy --name "$lab"
+  sudo containerlab destroy --topo "$lab"
 done
 
 echo "All running labs have been stopped."
